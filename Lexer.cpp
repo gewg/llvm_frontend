7dfce2ebd164e6b049ed4aa64991b85f3c29ab57
Aaron Ge <gewg046@outlook.com>
2025-03-08 15:48:44 +0800

    [Lexer] - Build the template of lexer

---
+++ Lexer.cpp
@@ -0,0 +1,24 @@
+/**
+ * The lexer, 接受程序，处理为Token
+ */
+
+
+/**
+ * Kaleidoscope language有以下token要被处理
+ * 1. def - string
+ * 2. extern - string
+ * 3. identifier - string
+ * 4. number - int or '.'
+ * 5. unknown - other
+ */
+// 声明所有token
+
+// Lexer入口
+
+// 声明numberVal和stringVal记录长度>1的input
+
+// 处理string
+
+// 处理number
+
+// 处理unknown, 直接返回他们的ASCII
